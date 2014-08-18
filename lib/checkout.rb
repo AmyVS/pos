36359382fb11779bb0be619ac1ba294b1b2ca2f1
Amy Vaillancourt-Sals & Andrew Robert Dean Akers <student@epicodus.com>
2014-08-18 15:54:46 -0700

    add a method for a customer to belong to a checkout

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,4 @@
 class Checkout < ActiveRecord::Base
   has_many :purchases
+  has_one :customer
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,11 +2,16 @@ require 'spec_helper'
 
 describe Checkout do
   it 'has many purchases' do
-    product1 = Product.create({:name => 'snowboard', :price => 450.0})
-    product2 = Product.create({:name => 'bindings', :price => 150.0})
     checkout = Checkout.create({:customer_id => 1, :cashier_id => 4})
-    purchase1 = Purchase.create({:product_id => product1.id, :checkout_id => checkout.id, :quantity => 1})
-    purchase2 = Purchase.create({:product_id => product2.id, :checkout_id => checkout.id, :quantity => 2})
+    purchase1 = Purchase.create({:product_id => 1, :checkout_id => checkout.id, :quantity => 1})
+    purchase2 = Purchase.create({:product_id => 2, :checkout_id => checkout.id, :quantity => 2})
     expect(checkout.purchases).to eq [purchase1, purchase2]
+  end
+
+  it 'has many customers' do
+    customer = Customer.create
+    cashier = Cashier.create
+    checkout = Checkout.create({:customer_id => customer.id, :cashier_id => cashier.id})
+    expect(checkout.customer_id).to eq customer.id
   end
 end
