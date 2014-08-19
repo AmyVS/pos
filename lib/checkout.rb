4905a7d00ce386e1d1a2a4045fbc335c01aa3f75
Amy Vaillancourt-Sals & Andrew Robert Dean Akers <student@epicodus.com>
2014-08-19 11:49:36 -0700

    add method for a checkout to have many products through purchases

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,4 +2,5 @@ class Checkout < ActiveRecord::Base
   has_many :purchases
   belongs_to :customer
   belongs_to :cashier
+  has_many :products, through: :purchases
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -8,6 +8,15 @@ describe Checkout do
     expect(checkout.purchases).to eq [purchase1, purchase2]
   end
 
+  it 'has many products through purchases' do
+    checkout = Checkout.create({:customer_id => 1, :cashier_id => 4})
+    product1 = Product.create({:name => 'fuzzy mittens', :price => 16.0})
+    product2 = Product.create({:name => 'fuzzy mittens', :price => 16.0})
+    purchase1 = Purchase.create({:product_id => product1.id, :checkout_id => checkout.id, :quantity => 1})
+    purchase2 = Purchase.create({:product_id => product2.id, :checkout_id => checkout.id, :quantity => 1})
+    expect(checkout.products).to eq [product1, product2]
+  end
+
   it 'belongs to a customer' do
     customer = Customer.create
     cashier = Cashier.create
